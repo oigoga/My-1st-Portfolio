dbebeeb1b4d038ee34696eb9c3fd4961b1c20f50
oigoga <gogaelisabeth21@gmail.com>
2023-01-11 23:42:41 +0100

    created the works page

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,7 @@
 import close from './close.svg'
 import menu from './menu.svg'
 import headshot from './Headshot.png'
-import resume from './Oigoga Aba_ Resume.pdf'
+import resume from  './Oigoga Aba Resume.pdf'
 
 export {
     close,
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,9 +2,61 @@ import React from 'react'
 
 const Works = () => {
   return (
-    <div>
-      Works
-    </div>
+   <section className='font-Ubuntu p-6'>
+      {/* <div className='md:px-6 md:mx-6 w-full flex-col md:mt-10 pb-20 xl:ml-20 md:pt-8 pt-24 lg:pl-10 p-6'>
+      <h4 className="text-4xl border-b-2 text-white md:text-6xl border-b-orange py-3">
+         My Portfolio
+        </h4>
+        <ul className='flex md:flex-row flex-col justify-center ml-5'>
+        <li className="flex flex-col md:flex-row  md:p-3  ">
+              <div className=" shadow-md shadow-gray w-3/4 md:w-1/2 text-xs rounded-md flex items-center flex-col h-80 md:m-3 m-3.5 justify-center">
+                 
+              </div>
+              <div className=" shadow-md shadow-gray w-3/4 md:w-1/2 text-xs rounded-md flex items-center flex-col h-80 m-3.5 md:m-3 justify-center">
+              
+              </div>
+            </li>
+            <li className="flex flex-col md:flex-row   md:p-3">
+              <div className=" shadow-md shadow-gray w-3/4 md:w-1/2 text-xs rounded-md flex items-center flex-col h-80 md:m-3 m-3.5 justify-center">
+                 
+              </div>
+              <div className=" shadow-md shadow-gray w-3/4 md:w-1/2 text-xs rounded-md flex items-center flex-col h-80 m-3.5 md:m-3 justify-center">
+              
+              </div>
+            </li>
+        </ul>
+      </div> */}
+
+      <div className=" md:w-2/3 flex  flex-col md:mt-10 pb-20 xl:ml-20 md:pt-8 pt-24 lg:pl-10 p-6 w-full mx-4">
+        
+        
+        <div className="font-Ubuntu w-full">
+        <h4 className="text-4xl border-b-2 text-white md:text-6xl border-b-orange py-3">
+         I Work With:
+        </h4>
+        <ul className="my-3 text-navy flex md:flex-row flex-col justify-center ">
+        
+            <li className="flex flex-col md justify-center md w-1/2 ">
+              <div className="shadow-md shadow-gray w-3/4 md:w-1/2 text-xs rounded-md flex items-center h-80 m-3.5 md:m-3 justify-center">
+             
+              </div>
+              <div className="shadow-md shadow-gray w-3/4 md:w-1/2 text-xs rounded-md flex items-center h-80 m-3.5 md:m-3 justify-center">
+              
+              </div>
+            </li>
+            <li className="flex flex-col justify-center md w-1/2 ">
+              <div className="shadow-md shadow-gray w-3/4 md:w-1/2 text-xs rounded-md flex items-center h-80 m-3.5 md:m-3 justify-center">
+              
+              </div>
+              <div className="shadow-md shadow-gray w-3/4 md:w-1/2 text-xs rounded-md flex items-center h-80 m-3.5 md:m-3 justify-center">
+              
+              </div>
+            </li>
+        </ul>
+        </div>
+        
+      </div>
+   </section>
   )
 }
 
