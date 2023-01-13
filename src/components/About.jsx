bc3710ac210c05500110427952dd4bdf4c87bd59
oigoga <gogaelisabeth21@gmail.com>
2023-01-13 11:02:04 +0100

    changed the dummy text in the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { DiGithubBadge, DiJsBadge, DiReact, DiCss3, DiHtml5, DiGit } from "react-icons/di";
+import {
+  DiGithubBadge,
+  DiJsBadge,
+  DiReact,
+  DiCss3,
+  DiHtml5,
+  DiGit,
+} from "react-icons/di";
 
 const About = () => {
   return (
@@ -9,89 +16,99 @@ const About = () => {
           About me
         </h4>
         <div className="bg-navyy rounded-md shadow-sm shadow-off-white my-4 p-3 text-white">
-        <p className="flex w-full my-3">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
-          tenetur dicta eos similique itaque dolor ea est possimus odit,
-          deserunt ratione aperiam velit, iure voluptate delectus inventore
-          unde, veniam beatae. Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Qui, soluta a repellendus laudantium molestiae
-          dolore adipisci dolorem quo sit voluptatibus nulla debitis officiis
-          fugit quas similique? Porro tempora quam aliquam! Lorem ipsum dolor
-          sit amet consectetur adipisicing elit. Non placeat cupiditate pariatur
-          ullam excepturi autem impedit, modi recusandae est fuga laudantium quo
-          voluptatibus velit enim optio id, voluptas fugit harum.
-        
-        </p>
+          <p className="flex w-full my-3 text-justify ">
+            Hi there I am Oigoga and I am a front-end developer. I have a knack
+            for learning quickly and this has helped me build my skillset over
+            the last couple of months.
+            </p> 
+             <p className="flex w-full my-3 text-justify my-3">
+             I am a resourceful, and result-oriented
+            person. I am great at organization and planning. I am enthusiastic
+            about being able to create stuff that people actually use and I am
+            passionate about aesthetically pleasing yet functional websites. I
+            am learning how to engage my mind to solve problems with critical
+            thinking and analysis. I am very open to learning new things to
+            achieve growth both as an individual and as part of any team. My
+            core values are discipline consistency and hard work as I have seen
+            with an immeasurable number of examples that they pay off with
+            incredible results.
+             </p>
+            
+             <p className="flex w-full my-3 text-justify my-3">
+             In addition to my technical skills. When I am
+            not coding, I’d either be reading, writing organizing my space or
+            life. I am also a certified technical writer. I am naturally
+            outspoken and I try to communicate assertively without any hint of
+            passive aggression. Lastly, I love cars, flavoured teas and clean
+            spaces.
+             </p>
+          
         </div>
-        
+
         <div className="font-Ubuntu w-full">
-        <h4 className="text-4xl border-b-2 text-white md:text-6xl border-b-orange py-3">
-         I Work With
-        </h4>
-        <ul className="my-3 text-navy flex md:flex-row flex-col justify-center  ">
-        <li className="flex  justify-center md ">
+          <h4 className="text-4xl border-b-2 text-white md:text-6xl border-b-orange py-3">
+            I Work With
+          </h4>
+          <ul className="my-3 text-navy flex md:flex-row flex-col justify-center  ">
+            <li className="flex  justify-center md ">
               <div className="bg-off-white shadow-md shadow-gray w-16 text-xs rounded-md flex items-center flex-col h-16 md:m-3 m-3.5 justify-center">
-              <div className=" my-3 w-5 rounded-md ">
-               <DiJsBadge className="h-full w-full rounded-sm"/>
-              </div>
-              <div className=" bg-gray-light w-full mb-0 rounded-md h-5 p-0.5 font-bold flex justify-center">
-                <span>Javascript</span>
-              </div>
+                <div className=" my-3 w-5 rounded-md ">
+                  <DiJsBadge className="h-full w-full rounded-sm" />
+                </div>
+                <div className=" bg-gray-light w-full mb-0 rounded-md h-5 p-0.5 font-bold flex justify-center">
+                  <span>Javascript</span>
+                </div>
               </div>
               <div className="bg-off-white shadow-md shadow-gray w-16 text-xs rounded-md flex items-center flex-col h-16 m-3.5 md:m-3 justify-center">
-              <div className=" my-3 w-5 rounded-md ">
-               <DiCss3 className="h-full w-full rounded-sm"/>
-              </div>
-              <div className=" bg-gray-light w-full mb-0 rounded-md h-5 p-0.5 font-bold flex justify-center ">
-                <span>CSS3</span>
-              </div>
+                <div className=" my-3 w-5 rounded-md ">
+                  <DiCss3 className="h-full w-full rounded-sm" />
+                </div>
+                <div className=" bg-gray-light w-full mb-0 rounded-md h-5 p-0.5 font-bold flex justify-center ">
+                  <span>CSS3</span>
+                </div>
               </div>
             </li>
             <li className="flex  justify-center md ">
               <div className="bg-off-white shadow-md shadow-gray w-16 text-xs rounded-md flex items-center flex-col h-16 m-3.5 md:m-3 justify-center">
-              <div className=" my-3 w-5 rounded-md ">
-               <DiHtml5 className="h-full w-full rounded-sm"/>
-              </div>
-              <div className=" bg-gray-light w-full mb-0 rounded-md h-5 p-0.5 font-bold flex justify-center ">
-                <span>HTML5</span>
-              </div>
+                <div className=" my-3 w-5 rounded-md ">
+                  <DiHtml5 className="h-full w-full rounded-sm" />
+                </div>
+                <div className=" bg-gray-light w-full mb-0 rounded-md h-5 p-0.5 font-bold flex justify-center ">
+                  <span>HTML5</span>
+                </div>
               </div>
               <div className="bg-off-white shadow-md shadow-gray w-16 text-xs rounded-md flex items-center flex-col h-16 m-3.5 md:m-3 justify-center">
-              <div className=" my-3 w-5 rounded-md ">
-               <DiReact className="h-full w-full rounded-sm"/>
-              </div>
-              <div className=" bg-gray-light w-full mb-0 rounded-md h-5 p-0.5 font-bold flex justify-center ">
-                <span>React</span>
-              </div>
+                <div className=" my-3 w-5 rounded-md ">
+                  <DiReact className="h-full w-full rounded-sm" />
+                </div>
+                <div className=" bg-gray-light w-full mb-0 rounded-md h-5 p-0.5 font-bold flex justify-center ">
+                  <span>React</span>
+                </div>
               </div>
             </li>
             <li className="flex  justify-center md ">
               <div className="bg-off-white shadow-md shadow-gray w-16 text-xs rounded-md flex items-center flex-col h-16 m-3.5 md:m-3 justify-center">
-              <div className=" my-3 w-5 rounded-md ">
-               <DiGit className="h-full w-full rounded-sm"/>
-              </div>
-              <div className=" bg-gray-light w-full mb-0 rounded-md h-5 p-0.5 font-bold flex justify-center ">
-                <span>Git</span>
-              </div>
+                <div className=" my-3 w-5 rounded-md ">
+                  <DiGit className="h-full w-full rounded-sm" />
+                </div>
+                <div className=" bg-gray-light w-full mb-0 rounded-md h-5 p-0.5 font-bold flex justify-center ">
+                  <span>Git</span>
+                </div>
               </div>
               <div className="bg-off-white shadow-md shadow-gray w-16 text-xs rounded-md flex items-center flex-col h-16 m-3.5 md:m-3 justify-center">
-              <div className=" my-3 w-5 rounded-md ">
-               <DiGithubBadge className="h-full w-full rounded-sm"/>
-              </div>
-              <div className=" bg-gray-light w-full mb-0 rounded-md h-5 p-0.5 font-bold flex justify-center  ">
-                <span>GitHub</span>
-              </div>
+                <div className=" my-3 w-5 rounded-md ">
+                  <DiGithubBadge className="h-full w-full rounded-sm" />
+                </div>
+                <div className=" bg-gray-light w-full mb-0 rounded-md h-5 p-0.5 font-bold flex justify-center  ">
+                  <span>GitHub</span>
+                </div>
               </div>
             </li>
-        </ul>
+          </ul>
         </div>
-        
       </div>
     </section>
   );
 };
 
 export default About;
-
-
-
